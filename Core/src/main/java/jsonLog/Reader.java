fc82bdfa030c39d284268a5b608a111c04a9b53c
Hannes <Hannes@Hannes-PC.localdomain>
2014-04-16 17:07:09 +0200

    test vom Laptop aus

--- Core/src/main/java/jsonLog/Reader.java
+++ Core/src/main/java/jsonLog/Reader.java
@@ -20,7 +20,7 @@ public class Reader {
 			    String username = "root";
 			    String password = "keanu7.";
 			    
-			 System.out.println("los juchei");
+			 System.out.println("los juchei nochmal");
 			 
 			 Connection connection;
 			try {
