151f0623b7ce6c0a89b940cb612ec7f958804a0f
Hannes Schuh <mail@hschuh.de>
2014-04-24 11:05:10 +0200

    same same

--- hi-wayDB/src/main/java/jsonLog/Reader.java
+++ hi-wayDB/src/main/java/jsonLog/Reader.java
@@ -10,36 +10,10 @@ import java.sql.Statement;
 public class Reader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		 try {
 			 
-			    String dbUrl = "jdbc:mysql://localhost/test";
-			    
-			    // String query = "Select distinct(table_name) from INFORMATION_SCHEMA.TABLES";
-			    String query = "SELECT * FROM test.new_table";
-			    String username = "root";
-			    String password = "keanu7.";
-			    
-			 System.out.println("los juchei nochmal und wieder zurückdd");
-			 
-			 Connection connection;
-			try {
-				connection = DriverManager.getConnection(dbUrl,
-				            username, password);
-				
-				 Statement statement = connection.createStatement();
-			        ResultSet resultSet = statement.executeQuery(query);
-			        while (resultSet.next()) {
-			        String tableName = resultSet.getString(2);
-			        System.out.println("Table name : " + tableName);
-			        }
-			        connection.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			       
-		 
+			    		 
 		 System.out.println("fertig...pressbutton");
 		
 			System.in.read();
