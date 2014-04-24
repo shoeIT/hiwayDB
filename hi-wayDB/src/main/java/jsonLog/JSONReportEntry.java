6f3e0414e999e709743850548147ae8d0d37bf4f
Hannes Schuh <mail@hschuh.de>
2014-04-24 11:37:32 +0200

    test

---
+++ hi-wayDB/src/main/java/jsonLog/JSONReportEntry.java
@@ -0,0 +1,13 @@
+package jsonLog;
+
+public class JSONReportEntry {
+	
+	public String entry;
+	
+	
+	public JSONReportEntry(String givenEntry){
+		entry = givenEntry;
+	}
+	
+	//public bool readInto
+}
