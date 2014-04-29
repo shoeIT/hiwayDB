5fde7996cf97f10fd521da7c4cf9427e06b1ccc6
Hannes <Hannes@Hannes-PC.localdomain>
2014-04-29 23:54:10 +0200

    log auslesen

--- hi-wayDB/src/main/java/jsonLog/Reader.java
+++ hi-wayDB/src/main/java/jsonLog/Reader.java
@@ -13,22 +13,26 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.json.JSONObject;
 
 import au.com.bytecode.opencsv.CSVReader;
+import dal.Invocation;
 import dal.Task;
+import dal.Workflowrun;
+import dal.WorkflowrunHasTask;
 
 public class Reader {
 
 	private static Path fFilePath;
 	private final static Charset ENCODING = StandardCharsets.UTF_8;
 	
-	private static long 
-
+	
 	public static void main(String[] args) {
 
 		try {
@@ -45,18 +49,85 @@ public class Reader {
 				String input = "D:\\Temp\\wordcount.cf.log";
 				
 				JsonReportEntry zeile;
+				
+				 UUID runId = null;
+				 Long invocId = null;
+				 String key = null;
+				 String value = null;
+				 String taskname = null;
+				 Long taskId = null;
+				 Long timestamp = null;
+				 String lang = null;
+				 JsonReportEntry payload = null;
+				 JSONObject valuePart = null;
+				 Invocation invoc = null;
+				 Workflowrun wfRun = null;
+				 WorkflowrunHasTask runTask= null;
+				 Task task = null;
+
 
 				if (input.endsWith(".log")) {
 
 					fFilePath = Paths.get(input);
+					
+					int i = 0;
 					try (Scanner scanner = new Scanner(fFilePath,ENCODING.name())) {
 						while (scanner.hasNextLine()) {
-							// processLine(scanner.nextLine());
+							i++;
+							System.out.println("line " + i);
 							zeile = new JsonReportEntry(scanner.nextLine());
 							
-							System.out.println(zeile.toString());
+							if(wfRun==null)
+							{
+								wfRun = new Workflowrun();
+								wfRun.setRunId(zeile.getRunId().toString());								
+							}
 							
-							zeile.get
+							if(task==null)
+							{
+								task = new Task();
+								//task(zeile.getRunId().toString());								
+							}
+							
+							if(runTask ==null)
+							{
+								runTask = new WorkflowrunHasTask();
+								//runTask.set
+							}
+							
+							System.out.println(zeile.toString());
+														
+							key = zeile.getKey();
+							
+							switch(key)
+							{
+							case "invoc-host":
+								
+								System.out.println(zeile.getValue());
+								if(invoc ==null)
+								{
+									invoc = new Invocation();
+									
+									invoc.setInvocationId(zeile.getInvocId());
+								}
+								else
+								{
+									invoc.setHostname(zeile.getValue());
+								}
+								
+								break;
+							case "invoc-time":
+								valuePart = zeile.getValueJsonObj();
+								System.out.println(valuePart.get("value"));
+								break;
+								
+							}
+//							payload = new JsonReportEntry(zeile.getValue());
+//							
+						String test2 = zeile.getValue();
+//							 = null;
+									
+						System.out.println(test2);						
 //													
 //							Session session = sessionFactory.openSession();
 //							session.beginTransaction();
@@ -78,19 +149,26 @@ public class Reader {
 //			}
 
 			
-			// Session session = sessionFactory.openSession();
-			// session.beginTransaction();
-			//
-			// Task task = new Task();
-			// task.setLanguage("egal");
-			// task.setTaskId(2);
-			// task.setTaskName("egal1");
-			//
-			// session.save(task);
-			// //session.save( new Event( "A follow up event", new Date() ) );
-			// session.getTransaction().commit();
-			// session.close();
-			//
+			 Session session = getDBSession().openSession();
+			 session.beginTransaction();
+			
+			 Task task1 = new Task();
+			 task1.setLanguage("egal");
+			 task1.setTaskId(2);
+			 task1.setTaskName("egal1");
+			
+			 
+			 Task task2 = new Task();
+			 task2.setLanguage("egal");
+			 task2.setTaskId(2);
+			 task2.setTaskName("egal1");
+			 
+			 session.save(task1);
+			 //session.save( new Event( "A follow up event", new Date() ) );
+			 session.getTransaction().commit();
+			
+			 session.close();
+			
 			
 
 			System.out.println("juchei");
@@ -100,6 +178,16 @@ public class Reader {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private static Invocation createInvoc(JsonReportEntry entry)
+	{
+		Invocation invoc = new Invocation();
+		
+		invoc.setInvocationId(entry.getInvocId());
+		
+		return invoc;
+		
 	}
 
 	private static SessionFactory getDBSession() {
